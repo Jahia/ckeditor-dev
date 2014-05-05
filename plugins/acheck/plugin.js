4f00bb02c83d1d03deb53dddcc844cebd0b8e091
Sergiy Shyrkov <sshyrkov@jahia.com>
2014-05-05 17:24:44 +0200

    Added Jahia's accessibility check plugin (acheck)

---
+++ plugins/acheck/plugin.js
@@ -0,0 +1,32 @@
+/**
+ * Digital Factory WCAG compiance check
+ * http://www.jahia.org/
+ * Copyright (C) 2002-2014 Jahia Solutions Group SA. All rights reserved.
+ */
+CKEDITOR.plugins.add('acheck',
+{
+    lang:'en,de,fr',
+    init: function(editor)
+    {
+        var pluginName = 'acheck',
+            commandName = 'acheck';
+        editor.addCommand(commandName, 
+            {
+        		modes : { wysiwyg : 1, source : 1 },
+		        exec : function(theEditor)
+	            {
+					if (typeof theEditor.checkWCAGCompliance == 'function') {
+						theEditor.checkWCAGCompliance(theEditor.name, document.getElementById(theEditor.name), true);
+					}
+	            },
+	            canUndo : false
+        });
+        
+        editor.ui.addButton('ACheck',
+            {
+        	label:editor.lang.acheck.title,
+        	command:commandName,
+        	icon:this.path+'acheck.gif'
+            });
+    }
+});
