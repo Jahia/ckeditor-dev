959e0b6d36222dad7dc0f901faeaab2dbb5bebdf
Thomas Draier <tdraier@jahia.com>
2014-09-12 10:52:22 +0200

    QA-6540 : added ckeditor configuration update script

--- config.js
+++ config.js
@@ -7,6 +7,25 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    CKEDITOR.DF_CONTEXT_PATH = (typeof contextJsParameters != 'undefined') ? contextJsParameters.contextPath : '';
+    CKEDITOR.DF_LANG = (typeof contextJsParameters != 'undefined') ? contextJsParameters.uilang : 'en';
+    CKEDITOR.DF_CONTENT_LANG = (typeof contextJsParameters != 'undefined') ? contextJsParameters.lang : 'en';
+    CKEDITOR.DF_SITE_UUID = (typeof contextJsParameters != 'undefined') ? contextJsParameters.siteUuid : '';
+    CKEDITOR.DF_CUSTOM_CONFIG = (typeof contextJsParameters != 'undefined') ? contextJsParameters.ckeCfg : '';
+
+    config.language = CKEDITOR.DF_LANG;
+
+    config.customConfig = CKEDITOR.DF_CUSTOM_CONFIG;
+
+    config.contentlanguage = CKEDITOR.DF_CONTENT_LANG;
+
+    config.siteUuid=CKEDITOR.DF_SITE_UUID;
+
+    config.filebrowserBrowseUrl=CKEDITOR.DF_CONTEXT_PATH + '/engines/contentpicker.jsp?site=' + CKEDITOR.DF_SITE_UUID + '&lang='+ CKEDITOR.DF_CONTENT_LANG + '&uilang='+ CKEDITOR.DF_LANG;
+    config.filebrowserImageBrowseUrl=CKEDITOR.DF_CONTEXT_PATH + '/engines/contentpicker.jsp?type=imagepicker&site=' + CKEDITOR.DF_SITE_UUID + '&lang='+ CKEDITOR.DF_CONTENT_LANG + '&uilang='+ CKEDITOR.DF_LANG;
+    config.filebrowserFlashBrowseUrl=CKEDITOR.DF_CONTEXT_PATH + '/engines/contentpicker.jsp?mime=application%2Fx-shockwave-flash%2Cvideo%2Fx-flv&site=' + CKEDITOR.DF_SITE_UUID + '&lang='+ CKEDITOR.DF_CONTENT_LANG + '&uilang='+ CKEDITOR.DF_LANG;
+    config.filebrowserLinkBrowseUrl=CKEDITOR.DF_CONTEXT_PATH + '/engines/contentpicker.jsp?type=editoriallinkpicker&site=' + CKEDITOR.DF_SITE_UUID + '&lang='+ CKEDITOR.DF_CONTENT_LANG + '&uilang='+ CKEDITOR.DF_LANG;
+
 	// %REMOVE_START%
 	config.plugins =
 		'about,' +
